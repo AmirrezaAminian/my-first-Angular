c3f04ce2572e3751f6d2cdf33cd01767182f2648
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-18 17:30:39 +0330

    first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // templateUrl: './servers.component.html',
+  template:'<app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
