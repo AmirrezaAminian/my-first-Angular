fafa7e3948465e947c7091cae1357edfc6af34f8
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-08-07 17:58:08 +0330

    Using Relative Paths in Programmic Navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
 
+import {Router , ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -9,10 +10,14 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService , private router:Router , private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    this.router.navigate(['servers'] , {relativeTo : this.route})
   }
 
 }
